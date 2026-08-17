474e19ec665f9e565c16183fd97b4b58b4ccbf16
Emmanuel Iheukwumere <mannyiheukwumere@gmail.com>
2026-08-17 16:06:42 +0100

    load schemas and create master build script

--- 2_DW_Mart_Build/02_load_schema_dw.sql
+++ 2_DW_Mart_Build/02_load_schema_dw.sql
@@ -4,16 +4,15 @@ select '=== Loading company_dim Table ===' as info;
 insert into company_dim(company_id,name)
 Select company_id,name
 from read_csv('https://storage.googleapis.com/sql_de/company_dim.csv',
-Auto_detect=true,parallel=false,  ignore_errors = true,
-    store_rejects = true);
-
+Auto_detect=true,parallel=false);
+ 
 
 select '=== Loading skills_dim Table ===' as info;
 
 insert into skills_dim(skill_id, skills, type)
 Select skill_id, skills, type
 from read_csv('https://storage.googleapis.com/sql_de/skills_dim.csv',
-Auto_detect=true,parallel=false,  ignore_errors = true,
+Auto_detect=true, parallel=false,  ignore_errors = true,
     store_rejects = true);
 
 
@@ -35,8 +34,7 @@ Select
       job_country, salary_rate, salary_year_avg,salary_hour_avg
 
 from read_csv('https://storage.googleapis.com/sql_de/job_postings_fact.csv',
-Auto_detect=true, parallel=false,  ignore_errors = true,
-    store_rejects = true,  types = {'salary_year_avg': 'VARCHAR'});
+Auto_detect=true, parallel=false);
 
 
 
@@ -44,9 +42,27 @@ select '=== Loading skills_job Table ===' as info;
 insert into skills_job_dim(skill_id, job_id)
 Select skill_id, job_id
 from read_csv('https://storage.googleapis.com/sql_de/skills_job_dim.csv',
-Auto_detect=true,parallel=false,  ignore_errors = true,
+Auto_detect=true, parallel=false,  ignore_errors = true,
     store_rejects = true);
 
+-- Validate data
+Select 'company dim' as table_name, count(*) as entry_rows from company_dim
+union all
+select 'skills dim', count(*) from skills_dim
+union all
+select 'Jobs Dim', count(*) from job_postings_fact
+union all
+select 'skills job dim', count(*) from skills_job_dim;
 
 
+select '=== Company Dimensions Sample ==' as info;
+select * from company_dim limit 5;
 
+select '=== Skills Dimension Sample ==' as info;
+select * from skills_dim limit 5;
+
+select '=== Job Postings Facat Sample ==' as info;
+select * from job_postings_fact limit 5;
+
+select '=== Skills job Dimension Sample ==' as info;
+select * from skills_job_dim limit 5;
